14ac4f6fd8a5e2ce0069d931f88c0b91772c744e
Chirag-varu <chiragvaru03@gmail.com>
2024-12-25 16:47:33 +0530

    Revert "bug fix"
    This reverts commit 50d941563b9a01719b34eebe20d5a0125fabaf6e.

--- server/src/routes/message.route.js
+++ server/src/routes/message.route.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { authenticate } from "../middleware/auth.middleware.js";
+import { auth } from "../middleware/auth.middleware.js";
 import { getUsers, getMessage, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.get("/users", authenticate, getUsers);
-router.get("/:id", authenticate, getMessage);
+router.get("/users", auth, getUsers);
+router.get("/:id", auth, getMessage);
 
-router.post("/send/:id", authenticate, sendMessage);
+router.post("/send/:id", auth, sendMessage);
 
 export default router;
