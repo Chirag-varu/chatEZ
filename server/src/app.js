892bd3c993ddae6d6cdb0a5ea02dc622686602b8
Chirag-varu <chiragvaru03@gmail.com>
2025-02-05 21:06:11 +0530

    production bug fix

--- server/src/app.js
+++ server/src/app.js
@@ -20,20 +20,20 @@ const limiter = rateLimit({
   message: "Too many requests from this IP, please try again after 15 minutes",
 });
 
+app.get("/healthcheck", (req, res) => {
+  res.status(200).send("Server is healthy");
+});
+
 // app.use(limiter);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
-
-app.get("/healthcheck", (req, res) => {
-  res.status(200).send("Server is healthy");
-});
 
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
