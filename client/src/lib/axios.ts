18dabf2dc0699aa672360dbb81698dbf93f6daba
Chirag-varu <chiragvaru03@gmail.com>
2025-04-02 10:51:19 +0530

    deployed on AWS amplify and EC2 instance

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,6 +2,6 @@ import axios, { AxiosInstance } from "axios";
 
 // Create an instance of Axios
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/v2" : "/api/v2",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/v2" : "http://18.204.226.1/api/v2",
   withCredentials: true,
 });
