a63fb781c219bb95917c74ea982c2291ad97f85d
Chirag-varu <chiragvaru03@gmail.com>
2025-04-02 21:02:12 +0530

    auto deployed with ci/cd pipeline

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,6 +2,6 @@ import axios, { AxiosInstance } from "axios";
 
 // Create an instance of Axios
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/v2" : "http://chatez.duckdns.org/api/v2",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/v2" : "/api/v2",
   withCredentials: true,
 });
